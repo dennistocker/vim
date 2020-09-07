1c11b53d072ce26814c1731bc1da77373728bfa5
dennistocker <dennistocker@gmail.com>
2020-09-07 14:13:04 +0800

    add screenrc

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cp ~/.vimrc ~/.vimrc_bak
-cp  vimrc ~/.vimrc
+# backup
+cp -f ~/.vimrc ~/.vimrc_bak
+cp -f ~/.screenrc ~/.screenrc_bak
+rm -rf ~/.vim/*
+
+# install
+cp  -f vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 cp bundles.vim ~/.vim/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
